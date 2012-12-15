396169dd1d7df86d144778bfb7c3024bdf097b9e
Sebastian Mitterle <sebastianmitterle@mathume.com>
2012-12-15 12:22:05 +0100

    unfinished work

--- drv/sql_driver.go
+++ drv/sql_driver.go
@@ -5,16 +5,20 @@ import (
 	"database/sql/driver"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"monet"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 const (
-	SEP      = "::::" //separator for connection string fields
-	DRV_NAME = "monet"
+	SEP                 = "::::" //separator for connection string fields
+	DRV_NAME            = "monet"
+	ROWS_BLOCK_SIZE     = 100
+	LAST_INSERT_ID_NONE = -1
 )
 
 var (
@@ -57,7 +61,7 @@ type MConn struct {
 
 func (c *MConn) Prepare(query string) (driver.Stmt, error) {
 	ni := strings.Count(query, "%")
-	return &MStmt{query, c.srv, ni, false}, nil
+	return &MStmt{query, c, ni, false}, nil
 }
 
 func (c *MConn) Close() error {
@@ -77,31 +81,60 @@ func (c *MConn) Begin() (driver.Tx, error) {
 }
 
 func (c *MConn) exec(command string) (string, error) {
+	if c.srv == nil {
+		return "", driver.ErrBadConn
+	}
 	return c.srv.Cmd("s" + command + ";")
+}
+
+func (c *MConn) cmd(operation string) (string, error) {
+	if c.srv == nil {
+		return "", driver.ErrBadConn
+	}
+	return c.srv.Cmd(operation)
+}
+
+func (c *MConn) sendReplySize() error {
+	_, err := c.cmd("Xreply_size " + string(ROWS_BLOCK_SIZE))
+	return err
 }
 
 type MStmt struct {
 	query  string
-	srv    monet.Server
+	conn   *MConn
 	ni     int
 	closed bool
 }
 
 func (s *MStmt) Close() error {
+	if s.closed {
+		return nil
+	}
+	s.conn = nil
+	s.closed = true
 	return nil
 }
 
 func (s *MStmt) Exec(args []driver.Value) (driver.Result, error) {
-	if len(args) != s.ni {
-		return nil, ErrNumInput
+	if err := s.isValid(args); err != nil {
+		return nil, err
 	}
-	
-	cmd := s.bind(args)
-
-	return nil, errors.New("stmt.exec not impl")
+	r := createMResult(s.conn)
+	err := r.exec(s.bind(args))
+	return &r, err
 }
 
-func (s *MStmt)bind(args []driver.Value) string{
+func (s *MStmt) isValid(args []driver.Value) error {
+	if len(args) != s.ni {
+		return ErrNumInput
+	}
+	if s.conn == nil {
+		return driver.ErrBadConn
+	}
+	return nil
+}
+
+func (s *MStmt) bind(args []driver.Value) string {
 	return fmt.Sprintf(s.query, args)
 }
 
@@ -110,13 +143,12 @@ func (s *MStmt) NumInput() int {
 }
 
 func (s *MStmt) Query(args []driver.Value) (driver.Rows, error) {
-	if len(args) != s.ni {
-		return nil, ErrNumInput
+	if err := s.isValid(args); err != nil {
+		return nil, err
 	}
-	
-	cmd := s.bind(args)
-	
-	return nil, errors.New("stmt.query not impl")
+	r := createMRows(s.conn)
+	err := r.query(s.bind(args))
+	return &r, err
 }
 
 type MTX struct {
@@ -133,8 +165,147 @@ func (t *MTX) Rollback() error {
 	return err
 }
 
-type MResult struct{
+type MResult struct {
+	conn *MConn
+	lid  int64
+	ra   int64
 }
 
-type MRows struct{
+func createMResult(conn *MConn) MResult {
+	return MResult{conn, LAST_INSERT_ID_NONE, 0}
+}
+
+func (r *MResult) exec(operation string) error {
+	if r.conn == nil {
+		return driver.ErrBadConn
+	}
+	if err := r.conn.sendReplySize(); err != nil {
+		return err
+	}
+	res, err := r.conn.exec(operation)
+	if err != nil {
+		return err
+	}
+	lines := strings.Split(res, "\n")
+	first := lines[0]
+
+	for strings.HasPrefix(first, monet.MSG_INFO) {
+		lines = lines[1:]
+		first = lines[0]
+	}
+
+	if strings.HasPrefix(res, monet.MSG_QUPDATE) {
+		sai := SplitWS(first[2:])
+		r.ra, err = strconv.ParseInt(sai[0], 10, 64)
+		r.lid, err = strconv.ParseInt(sai[1], 10, 64)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if strings.HasPrefix(res, monet.MSG_QSCHEMA) {
+		return nil
+	}
+
+	if strings.HasPrefix(res, monet.MSG_QTRANS) {
+		return nil
+	}
+
+	if strings.HasPrefix(res, monet.MSG_PROMPT) {
+		return nil
+	}
+
+	if strings.HasPrefix(res, monet.MSG_ERROR) {
+		return errors.New(first[1:])
+	}
+
+	return errors.New("Unknown state " + first)
+}
+
+func (r *MResult) LastInsertId() (int64, error) {
+	return r.lid, nil
+}
+
+func (r *MResult) RowsAffected() (int64, error) {
+	return r.ra, nil
+}
+
+type MRows struct {
+	conn     *MConn
+	rows     [][]driver.Value
+	curr     int
+	cols     []string
+	query_id string
+	offset   int
+}
+
+func createMRows(conn *MConn) MRows {
+	return MRows{conn, nil, -1, *new([]string), "", 0}
+}
+
+func (r *MRows) query(operation string) error {
+	if r.conn == nil {
+		return driver.ErrBadConn
+	}
+	if err := r.conn.sendReplySize(); err != nil {
+		return err
+	}
+	res, err := r.conn.exec(operation)
+	if err != nil {
+		return err
+	}
+	lines := strings.Split(res, "\n")
+	first := lines[0]
+
+	for strings.HasPrefix(first, monet.MSG_INFO) {
+		lines = lines[1:]
+		first = lines[0]
+	}
+
+	if strings.HasPrefix(first, monet.MSG_QTABLE){
+		meta := SplitWS(first[2:])
+		
+	}
+	return errors.New("Unknown state " + first)
+}
+
+func (r *MRows) Close() error {
+	if r.conn != nil{
+		r.conn = nil
+	}
+	return nil
+}
+
+func (r *MRows) Columns() []string {
+	return r.cols
+}
+
+func (r *MRows) Next(dest []driver.Value) error {
+	r.curr++
+	if r.curr > len(r.cols) {
+		return io.EOF
+	}
+	if len(dest) < len(r.cols) {
+		return errors.New("Not enough space in dest slice.")
+	}
+	copy(dest, r.rows[r.curr])
+	return nil
+}
+
+func SplitWS(s string) []string{
+	res := make([]string, 0)
+	if s == "" {
+		return res
+	}
+	s = strings.Replace(s, "\t", " ", -1)
+	s = strings.Replace(s, "\n", " ", -1)
+	s = strings.Replace(s, "\r", " ", -1)
+	ss := strings.Split(s, " ")
+	for _,s := range ss {
+		if strings.Trim(s, " ") != "" {
+			res = append(res, s)
+		}
+	}
+	return res
 }
--- drv/sql_driver_test.go
+++ drv/sql_driver_test.go
@@ -1,12 +1,12 @@
-package drv_test
+package drv
 
 import (
 	"testing"
 	"database/sql"
 	"log"
 	"time"
+//	"io"
 	"monet"
-	. "monet/drv"
 	. "launchpad.net/gocheck"
 )
 
@@ -41,23 +41,75 @@ func (d *DRIVER)TestErrorAtBadConnString(c *C){
 }
 
 func (d *DRIVER)TestPrepare(c *C){
-	dr, _ := Open(validConnString)
+	dr, _ := new(MDriver).Open(validConnString)
 	s, err := dr.Prepare("anyQuery")
+	c.Log(s)
+	c.Log(err)
 	c.Error("not finished yet")	
 }
+/*
 
 func (d *DRIVER)TestMTX(c *C){
-	c.Error("not finished yet")
+	t := new (MTX)
+	t.c = new(fakeConn)
+	t.Commit()
+	c.Assert(t.query, Equals, "COMMIT")
+	t.Rollback()
+	c.Assert(t.query, Equals, "ROLLBACK")
 }
 
+func (d *DRIVER)TestMRows(c *C){
+	row1 := []int64{1,2}
+	row2 := []int64{3,4}
+	rows := [][]int64{row1, row2}
+	rr := &MRows{nil, rows, -1, []string{"col1", "col2"}, "anyId", 0}
+	c.Assert(rr.Columns()[1], Equals, "col2")
+	dest := make([]int64) //too short
+	err := rr.Next(dest)
+	c.Assert(err, Not(IsNil))
+	dest := make([]int64, len(rr.Columns()))
+	err := rr.Next(dest)
+	c.Assert(err, IsNil)
+	c.Assert(dest[0], Equals, 1)
+	err = rr.Next(dest)
+	c.Assert(err, IsNil)
+	c.Assert(dest[1], Equals, 3)
+	err = rr.Next(dest)
+	c.Assert(err, Equals, io.EOF)
+	err = nil
+	err = rr.Next(dest)
+	c.Assert(err, Equals, io.EOF)
+}
+*/
+func (d *DRIVER)TestStrip(c *C){
+	s := "a b\tc\rd\ne \t\r\nf"
+	ss := SplitWS(s)
+	s = ""
+	for _,v := range ss {
+		s += string(v)
+		c.Log(string(v))
+	}
+	c.Log(s)
+	c.Assert(len(ss), Equals, 6)
+	c.Assert(s, Equals, "abcdef");
+}
+
+type fakeConn struct{
+	MConn
+	query string
+}
+
+func (c *fakeConn)exec(query string){
+	c.query = query
+}
 
 type LIVE struct{}
 
 var _ = Suite(&LIVE{})
 
 func (l *LIVE)TestErrorAtBadConnString1(c *C){
-	dr, _ := sql.Open(DRV_NAME, defaultValidConnString)
-	
+	dr, _ := sql.Open(DRV_NAME, validConnString)
+
 	_, err := dr.Exec("select count(*) from voc.craftsmen")
 	c.Assert(err, Not(Equals), ErrConnString)
 }
