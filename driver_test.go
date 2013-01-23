38c013a9e10cb771f6d7225ee3b6d753496014c2
Sebastian Mitterle <sebastianmitterle@mathume.com>
2013-01-23 22:05:24 +0100

    added tests for connection

--- driver_test.go
+++ driver_test.go
@@ -15,3 +15,40 @@ func (d *DRIVER) SetUpSuite(c *C) {
 func (d *DRIVER) SetUpTest(c *C) {
 	logger.Clear()
 }
+
+func (d *DRIVER)TestBeginSendsStartTransaction(c *C){
+	con := new(mconn)
+	con.srv = fakeServer
+	//setup
+	con.Begin()
+	c.Assert(fakeServer.Received(), Equals, "sSTART TRANSACTION;")
+}
+
+func (d *DRIVER)TestBeginCanOnlyBeCalledOnce(c *C){
+	con := new(mconn)
+	con.srv = fakeServer
+	//setup
+	tx, err := con.Begin()
+	c.Assert(err, IsNil)
+	c.Assert(tx, Not(IsNil))
+	tx, err = con.Begin()
+	c.Assert(tx, IsNil)
+	c.Assert(err, Not(IsNil))
+}
+
+func (d *DRIVER)TestCloseWillRollbackWhenThereIsATx(c *C){
+	con := new(mconn)
+	con.srv = fakeServer
+	//setup
+	con.Begin()
+	con.Close()
+	c.Assert(contains(fakeServer.Received(), "sROLLBACK;"), Equals, true)
+}
+
+func (d *DRIVER)TestCloseWillNotRollbackWithoutTx(c *C){
+	con := new(mconn)
+	con.srv = fakeServer
+	//setup
+	c.Close()
+	c.Assert(contains(fakeServer.Received(), "sROLLBACK;"), Equals, false)
+}
